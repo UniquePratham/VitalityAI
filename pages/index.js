b1ef40ac04791c0899f676a98d8f87c766157c70
Pratham Agarwal <gargpratham71@gmail.com>
2024-08-24 15:26:08 +0530

    Work Update 1

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import Footer from "@/components/Footer";
+import FAQ from "@/components/FAQ";
+import Guide from "@/components/Guide";
+const Navbar = dynamic(() => import("@/components/Navbar"), {
+  ssr: false,
+});
+const HeroSection = dynamic(() => import("@/components/HeroSection"), {
+  ssr: false,
+});
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Guide />
+      <FAQ />
+      <Footer />
+    </>
+  );
+}
